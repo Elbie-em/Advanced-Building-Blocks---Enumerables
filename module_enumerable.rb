58d7bdc61ffea494b692d9e836121107cb526c47
elbie-em <emoonga1993@gmail.com>
2020-03-11 18:18:24 +0100

    added my_each method to module

--- module_enumerable.rb
+++ module_enumerable.rb
@@ -5,7 +5,27 @@
 
 # This Module consists of replicated enumerable methods in RUBY
 module Enumerable
-  
+  def my_each
+    return to_enum :my_each unless block_given?
+
+    obj = self
+    itr = 0
+    loop do
+      case obj
+      when Array
+        yield(obj[itr])
+        itr += 1
+        break if itr == obj.length
+      when Hash
+        yield(obj.keys[itr], obj[obj.keys[itr]])
+        itr += 1
+        break if itr == obj.size
+      else
+        obj
+      end
+    end
+    obj
+  end
 end
 
 # rubocop:enable Metrics/MethodLength
