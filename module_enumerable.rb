6ac9e98e8f41994f6b9b4dcf55a589b950aa0717
elbie-em <emoonga1993@gmail.com>
2020-03-11 18:12:45 +0100

    added own Enumerable module to file

---
+++ module_enumerable.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/MethodLength
+# rubocop:disable Metrics/CyclomaticComplexity
+
+# This Module consists of replicated enumerable methods in RUBY
+module Enumerable
+  
+end
+
+# rubocop:enable Metrics/MethodLength
+# rubocop:enable Metrics/CyclomaticComplexity
